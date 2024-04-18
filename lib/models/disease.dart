75b5e854ed4bdbcab09c3f23ce5b14ed5f0c435a
Tushar <tusharkva8a@gmail.com>
2024-04-18 22:59:52 +0530

    f

--- lib/models/disease.dart
+++ lib/models/disease.dart
@@ -1,6 +1,4 @@
-class Disease{
-  final String name,hindiName,description,hindiDescription;
-  Disease({
-    required this.name
-  })
-}
+// class Disease{
+//   String name,hindiName,description,hindiDescription;
+//
+// }
